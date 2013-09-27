5af255a797ac471dec795efbbfcf9bb8eb58bf43
fz1989 <fz1989fz@gmail.com>
2013-09-27 16:53:01 +0800

    finish front page

--- start.py
+++ start.py
@@ -1,17 +1,30 @@
 #! /usr/bin/env python
 # coding: UTF-8
+import tornado
 import tornado.ioloop
 import tornado.web
+import os
+import tornado.httpserver
+
 
 class HomeHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.render("base.html")
 
 
-application = tornado.web.Application([
-    (r"/home", HomeHandler),
-])
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", HomeHandler),
+        ]
+        settings = dict(
+            template_path=os.path.join(os.path.dirname(__file__), "app/templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "app/assets"),
+            debug=True,
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
 
 if __name__ == "__main__":
-    application.listen(8080)
+    application = Application()
+    application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
